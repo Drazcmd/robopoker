225088b5f1f9656060b88157952120eeb1dab0f2
CD <Drazcmd@users.noreply.github.com>
2025-05-14 22:25:44 -0500

    Highlight that t=0 for river + preflop
    I was confused at first as to why the clustering was "instant" + not actually hitting my TODO. Then I realized, t=0 for the first street that's calculated (river)! See street.rs definition for fn t:
    
    ```
        pub const fn t(&self) -> usize {
            match self {
                Self::Pref => 0,
                Self::Flop => crate::KMEANS_FLOP_TRAINING_ITERATIONS,
                Self::Turn => crate::KMEANS_TURN_TRAINING_ITERATIONS,
                Self::Rive => 0,
            }
        }
    ```
    
    (This is also why there are only definitions for KMEANS_FOO_TRAINING_ITERATIONS for FLOP and TURN over in lib.rs. I.e. they're implicitly 0.)
    
    Given the lack of tests or benchamrks for it, to actually exercise the layer.rs clustering code therefore means waiting for the river to finish up and then for it to move on to the turn (I think).
    
    (Also: figured out how to run this on shortdeck, it's pretty simple: just do `cargo run --features shortdeck`)

--- src/clustering/layer.rs
+++ src/clustering/layer.rs
@@ -84,6 +84,7 @@ impl Layer {
         log::info!("{:<32}{:<32}", "clustering  kmeans", self.street());
         let t = self.street().t();
         let progress = crate::progress(t);
+        log::info!("{:<32}{:<32})","  - #training iterations 't':", t);
 
         // Initialization from Elkan (2003) immediately prior to the 7-step
         // triangle inequality-based accelereated k-means algorithm.
@@ -119,6 +120,8 @@ impl Layer {
 
         let triangle_accelerate_todo_replaceme = false;
         for _ in 0..t {
+            // log::info!("Training iteration {:<32}", t);
+
             if triangle_accelerate_todo_replaceme {
                 let (ref next_kmeans, ref next_helpers) =
                     self.cluster_step_triaccl(&triangle_inequality_helpers);
