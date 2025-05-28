2c2831f9c4ab78162cc3df90dfa180fa59a7ee5e
CD <Drazcmd@users.noreply.github.com>
2025-05-28 00:39:06 -0700

    Oops
    Forgot to fmt + test and had a typo

--- src/clustering/layer.rs
+++ src/clustering/layer.rs
@@ -424,7 +424,10 @@ impl Layer {
         // we're using fancy `Histogram`s which need more complicated math to
         // compute the distance, as opposd to just doing standard euclidian
         // distance calculations.)
-        log::info!("{:<32}", " - STEP 3, getting working points (remove me later)");
+        log::info!(
+            "{:<32}",
+            " - STEP 3, getting working points (remove me later)"
+        );
         let mut step_3_working_points: HashMap<usize, (&Histogram, TIBounds)> = self
             .points()
             .iter()
@@ -440,7 +443,7 @@ impl Layer {
         // parallelized code down below inside this outer loop.)
         log::info!("{:<32}", " - STEP 3, starting outer loop (remove me later)");
         for (center_c_idx, center_c) in self.kmeans().iter().enumerate().collect::<Vec<_>>() {
-            log::info!("{:<24}", "{:<8}", " - STEP 3, outer loop #", center_c_idx);
+            log::info!("{:<24}{:<8}", " - STEP 3, outer loop #", center_c_idx);
             let immutable_step_3_working_points = step_3_working_points.clone();
             for (point_i, point_h, helper) in immutable_step_3_working_points
                 .par_iter()
